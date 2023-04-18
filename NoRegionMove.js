73cea07d368c71895d8124d7413fed3d4daa3e31
Rootabeta <rootabeta@protonmail.com>
2023-04-18 16:56:16 -0400

    Create NoRegionMove.js

---
+++ NoRegionMove.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         No region move
+// @version      0.2
+// @namespace    dithpri
+// @description  prevent moving regions on defined nations
+// @author       rootabeta stole from dithpri stole with permsion by 9003
+// @noframes
+// @match        https://www.nationstates.net/*region=*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var nations = ['volstrostia','someotherNation'];
+    if (nations.includes(document.body.dataset.nname)) {
+    // if (document.body.dataset.nname == 'volstrostia') { //Old code was limited to one nation
+        document
+            .querySelectorAll('button[name="move_region"]')
+            .forEach(function (el, idx) {
+            el.disabled = true;
+        });
+    }
+})();
