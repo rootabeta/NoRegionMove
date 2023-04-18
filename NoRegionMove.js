d815aaefe9bd43ab1d7005cf198f68e35f236a55
Rootabeta <rootabeta@protonmail.com>
2023-04-18 17:56:01 -0400

    template=none fix
    Terrible, horrible, no-good, very bad kludge.

--- NoRegionMove.js
+++ NoRegionMove.js
@@ -3,20 +3,32 @@
 // @version      0.2
 // @namespace    dithpri
 // @description  prevent moving regions on defined nations
-// @author       rootabeta stole from dithpri stole with permsion by 9003
+// @author       dithpri stole with permsion by 9003
 // @noframes
 // @match        https://www.nationstates.net/*region=*
+// @match        https://www.nationstates.net/*region=*/
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var nations = ['volstrostia','someother_nation']; //DO NOT FORGET TO MAKE LOWERCASE AND UNDERSCORE FOR SPACES
-    if (nations.includes(document.body.dataset.nname)) {
-    // if (document.body.dataset.nname == 'volstrostia') { //Old code was limited to one nation
-        document
+    var nations = ['volstrostia','kalist_halishir','genderdruid','kingsbane','a_pawn','the_quiet','the_valhalla_hotel','alexs_region','dedsec_has_given_you_the_truth','lieutenant_murphy','a_polychromatic_inmate'];
+
+    var currentNation = document.body.dataset.nname;
+    if (currentNation) {
+        if (nations.includes(document.body.dataset.nname)) {
+            // if (document.body.dataset.nname == 'volstrostia') {
+            document
             .querySelectorAll('button[name="move_region"]')
             .forEach(function (el, idx) {
             el.disabled = true;
-        });
+            });
+        }
+    } else { //Horrible, awful, evil hack. BUT IT WORKS!!!
+        console.log("Could not identify current nation. Activating contingency plan.");
+            // Move Big Nation to Suspicious
+            // My contingency plan is awful - find the move button, and if nsifying the text and removing the Move_ reveals our_nation_to_ (not just our_nation) then we can move safely
+        var regionName = document.getElementsByName("region_name")[0].value; //pre_nsified :)
+
+        document.querySelectorAll('button[name="move_region"]').forEach(function (el, idx) { var buttonText = el.children[0].nextSibling.wholeText.toLowerCase().replace(/ /gi,"_") ; console.log(regionName); var nationName = buttonText.substring(5,buttonText.length-(regionName.length + 5)); console.log(nationName) ; if (nations.includes(nationName)) {el.disabled = true;} });
     }
 })();
