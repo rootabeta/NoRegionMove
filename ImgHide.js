ebfb778cef5ac10f5397cc3354eb125d001d2e74
Rootabeta <rootabeta@protonmail.com>
2023-05-04 11:44:30 -0400

    Create ImgHide.js

---
+++ ImgHide.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Template Image Blocker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Block images from loading when template-overall=none is applied
+// @author       rootabeta
+// @match        https://www.nationstates.net/*/template-overall=none
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nationstates.net
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.querySelectorAll("img").forEach(function (el, idx) { el.remove(); }); //This does a good job of removing images, which helps unclutter the screen, but does not prevent them from downloading
+})();
